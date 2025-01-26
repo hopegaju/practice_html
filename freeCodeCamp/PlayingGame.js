4bb28566a3a0dfe5598f896630ad5b7d1011c5d0
hopegaju <hopegajupr@gmail.com>
2025-01-26 19:49:55 +0200

    new

---
+++ freeCodeCamp/PlayingGame.js
@@ -0,0 +1,25 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+
+const locations = [
+  {
+    name: "town square",
+    buttonText: "Visit shop",
+    text: "You enter the town square. There's a shop selling magic items.",
+    buttonFunction: visitShop,
+  },
+]
